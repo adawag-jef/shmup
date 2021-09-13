800e785fe8550fba207eb88d8b21052e6b900638
adawag-jef <epyadawag@gmail.com>
2021-09-13 16:37:53 +0800

    add sound effects

--- shmup.py
+++ shmup.py
@@ -22,6 +22,7 @@ pygame.display.set_caption("Shmup")
 clock = pygame.time.Clock()
 
 img_dir = path.join(path.dirname(__file__), 'image')
+snd_dir = path.join(path.dirname(__file__), 'snd')
 
 
 # load images
@@ -44,6 +45,14 @@ meteor_list = [
 
 for img in meteor_list:
     meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+
+# load audio
+shoot_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav'))
+expl_sounds = []
+for snd in ['expl3.wav', 'expl6.wav']:
+    expl_sounds.append(pygame.mixer.Sound(path.join(snd_dir, snd)))
+pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.4)
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -88,9 +97,11 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = 0
     
     def shoot(self):
+
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
+        shoot_sound.play()
 
 
 class Mob(pygame.sprite.Sprite):
@@ -160,6 +171,7 @@ for i in range(9):
     mobs.add(m)
 
 score = 0
+pygame.mixer.music.play(loops=-1)
     
 # Game loop
 running = True
@@ -183,6 +195,7 @@ while running:
 
     for hit in hits:
         score += 50 - hit.radius
+        random.choice(expl_sounds).play()
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
